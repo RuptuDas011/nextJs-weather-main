84dcef57844dd2e7053b4851bfdd99e38f7e4c79
Ruptu011 <ruptudas2@gmail.com>
2024-12-03 12:46:12 +0530

    new tag new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,21 +19,23 @@ export default function RootLayout({
     <html lang="en">
       <head>
    
-<script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WQPM6KJB')`};</script>
+})(window,document,'script','dataLayer','GTM-PN88LT68')`};</script>
 
 
       </head>
-       <GoogleTagManager gtmId="GTM-WQPM6KJB" />
+       {/* <GoogleTagManager gtmId="GTM-WQPM6KJB" /> */}
       <body className={inter.className}>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQPM6KJB"
-height="0" width="0" style={{display:"none",visibility:"hidden"}}>
+     
+<noscript>
+  <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PN88LT68"
+height="0" width="0" style={{display:'none',visibility:'hidden'}}>
   </iframe>
   </noscript>
+
 
 {children}
 
